129fe22d707c8f68910f94e0d17395e825af6ea9
Marty <marty310459@g.ncu.edu.tw>
2026-03-22 14:36:25 +0800

    Initial commit for Dice Roller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -35,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,7 +50,8 @@ class MainActivity : ComponentActivity() {
             DiceRollerTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    //color = MaterialTheme.colorScheme.background
+                    color = Color.Blue
                 ) {
                     DiceRollerApp()
                 }
